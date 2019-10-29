0bb10765fef02cc852edb9c000b40afb9202152a
TianhaoQian <tianhao.qian@ericsson.com>
2019-10-29 14:27:20 +0800

    modified RuntimeDemo.java and JvmNote.txt

--- src/jvm/RuntimeDemo.java
+++ src/jvm/RuntimeDemo.java
@@ -12,8 +12,11 @@ public class RuntimeDemo {
          * -Xmx:最大分配内存，默认为物理内存的1/4
          * -Xms:初始分配大小，默认为物理机内存的1/64
          * 注：生产环境下-Xmx和-Xms大小必须一致！避免GC跟应用程序争抢内存，产生停顿
+         *
+         * VM参数：   -Xms1024m -Xmx1024m -XX:+PrintGCDetails
          */
         System.out.println(runtime.maxMemory()/(double)1024/1024); // -Xmx java虚拟机可以使用最大内存量
         System.out.println(runtime.totalMemory()/(double)1024/1024); // -Xms java虚拟机内存总量
+
     }
 }
