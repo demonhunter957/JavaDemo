479a08b87c69c0c16dd1f55a97c0a9d46c863b59
TianhaoQian <tianhao.qian@ericsson.com>
2019-12-22 17:07:25 +0800

    add fiels

---
+++ src/designPattern/singleton/SingletonDemo01.java
@@ -0,0 +1,29 @@
+package designPattern.singleton;
+
+/**
+ * 双重检查单例模式
+ * @author Chris
+ * @create 2019-12-11 13:29
+ */
+public class SingletonDemo01 {
+
+    //构造器私有化
+    private SingletonDemo01(){}
+
+    //创建一个初始值为空的私有静态属性
+    private static SingletonDemo01 instance;
+
+    //对外提供构造方法
+    public static SingletonDemo01 getInstance(){
+        if (null == instance){
+            synchronized (SingletonDemo01.class){
+                if (null == instance){
+                    instance = new SingletonDemo01();
+                }
+            }
+        }
+        return instance;
+    }
+
+}
+
--- src/stream/FunctionalInterface.java
+++ src/stream/FunctionalInterface.java
@@ -13,19 +13,26 @@ public class FunctionalInterface {
     public static void main(String[] args) {
 
         // 1. 函数型接口：Function<T, R>，接受一个参数T返回一个参数R
+        // new这个接口的时候重写了apply(String s) 方法
         Function<String , Integer> function = s -> {return s.length();}; // 也可以写成s -> s.length()
         System.out.println(function.apply("Chris"));
 
         // 2. 断定型接口：Predicate<T>，接受一个参数T返回一个boolean
+        // new这个借口的时候重写了 test(String s)方法
         Predicate<String> predicate = s -> s.isEmpty();
         System.out.println(predicate.test("Chris"));
 
+
         // 3. 消费型接口：Consumer<T>，接受一个参数T没有返回值
+        // new这个接口的时候重写了accept(String s)方法
         Consumer<String> consumer = s ->  {System.out.println(s);};
         consumer.accept("Chris");
 
         // 4. 供给型接口：Supplier<T>，不接受参数返回一个参数T
-        Supplier<String> supplier = () -> "ChrisQian";
+        // new这个接口的时候重写了get()方法
+        Supplier<String> supplier = () -> "Chris";
         System.out.println(supplier.get());
+
+
     }
 }
